253a195e1e1887d7566aca42e0a53ffd1a559b20
Josh Sullivan <jaysche.sullivan@gmail.com>
2018-05-10 23:44:27 -0400

    More spotlight manager work

--- projects/ngx-onboarding/src/lib/components/manager/manager.component.ts
+++ projects/ngx-onboarding/src/lib/components/manager/manager.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterContentInit,
+  Optional,
+  ChangeDetectorRef,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { AnimationEvent } from '@angular/animations';
+import { CdkStepper, StepContentPositionState } from '@angular/cdk/stepper';
 import { Select, Store } from '@ngxs/store';
+import { Directionality } from '@angular/cdk/bidi';
 
 @Component({
   selector: 'obd-manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
-export class ManagerComponent implements OnInit {
+export class ManagerComponent extends CdkStepper implements OnInit, AfterContentInit {
   /**
    * For toggling the overlay "blanket" transparency
    * @type {boolean}
@@ -19,7 +31,30 @@ export class ManagerComponent implements OnInit {
    */
   @Input() children: Node;
 
-  constructor(private store: Store) {}
+  /**
+   * Event emitted when the current step is done transitioning in.
+   * @type {EventEmitter<void>}
+   */
+  @Output() readonly animationDone: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(
+    private store: Store,
+    @Optional() dir: Directionality,
+    changeDetectorRef: ChangeDetectorRef
+  ) {
+    super(dir, changeDetectorRef);
+  }
 
   ngOnInit() {}
+
+  ngAfterContentInit(): void {}
+
+  /**
+   * @param event
+   */
+  animationDoneHandler(event: AnimationEvent) {
+    if ((event.toState as StepContentPositionState) === 'current') {
+      this.animationDone.emit();
+    }
+  }
 }
